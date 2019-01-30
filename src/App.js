1c37401330d841952b87f39c07454b8af3d91cb9
Jeff Kushner <jeff.kushner@me.com>
2019-01-30 12:58:57 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -7,10 +7,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
 }
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
 
 export default App;

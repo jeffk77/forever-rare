252a7a7ce6dc3ac4c46e30f70cb7187378fe066a
jeffk77 <jeff.kushner@me.com>
2019-03-13 17:02:00 -0400

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Logo from '../assets/images/FRDlogo.png';
 
 const Footer = () => (
   <div>
-
+      <footer>
+        <div className="row">
+          <div className="col l2 m2 hide-on-med-and-down">
+            Column 1
+          </div>
+          <div className="col l8 m12 s12">
+            Column 2
+          </div>
+          <div className="col l2 m2 hide-on-med-and-down">
+            <h6>info@forever-rare.com<br />
+            416-363-5825</h6>
+          </div>
+        </div>
+      </footer>
   </div>
 );
 
